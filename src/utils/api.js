8666c2814602ec0a7f286308b50ccfb84df3fc9f
Irshad <irshadsap6644@gmail.com>
2025-11-17 05:23:28 -0800

    connected correct backend

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,16 +1,1 @@
-const BASE_URL = "http://localhost:5000";  
-
-export const fetchSalesToday = async () => {
-  const res = await fetch(`${BASE_URL}/api/sales-today`);
-  return res.json();
-};
-
-export const fetchTotalBills = async () => {
-  const res = await fetch(`${BASE_URL}/api/total-bills`);
-  return res.json();
-};
-
-export const fetchTotalCustomers = async () => {
-  const res = await fetch(`${BASE_URL}/api/total-customers`);
-  return res.json();
-};
+export const backendURL = "https://backend-billmitra.onrender.com";
